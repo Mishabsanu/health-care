8ca1bf6ffbd92b27d3326340f4c33e9309f2b4e0
mishab <mishabsanu298@gmail.com>
2026-04-12 19:18:16 +0530

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://health-care-umber-five.vercel.app/api'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://health-care-backend-nlre.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_URL,
